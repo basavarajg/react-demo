545df19b204a1a2de60f1415210c796b010cd0ea
Basavaraj G <basu@Basavarajs-MacBook-Pro.local>
2017-04-17 01:16:38 -0400

    committing new files for the web application

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
+//import AwesomeComponent from './AwesomeComponent.jsx';
+import Resume from './resume.jsx';
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <p> Hello React!</p>
-        <AwesomeComponent />
+        {/* <AwesomeComponent /> */}
+        <Resume />
       </div>
     );
   }
